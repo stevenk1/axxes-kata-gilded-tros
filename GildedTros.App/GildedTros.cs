2759fd7d81950de6d2c641081177314eda54bb25
Steven Kennis <steven.kennis1@gmail.com>
2025-01-02 00:42:03 +0100

    approvaltest is green, can delete legacy code

--- GildedTros.App/GildedTros.cs
+++ GildedTros.App/GildedTros.cs
@@ -1,95 +1,22 @@
 ﻿using System.Collections.Generic;
 
-namespace GildedTros.App
+namespace GildedTros.App;
+
+public class GildedTros
 {
-    public class GildedTros
+    private readonly IList<Item> Items;
+
+    public GildedTros(IList<Item> Items)
     {
-        IList<Item> Items;
-        public GildedTros(IList<Item> Items)
+        this.Items = Items;
+    }
+
+    public void UpdateQuality()
+    {
+        foreach (var item in Items)
         {
-            this.Items = Items;
-        }
-
-        public void UpdateQuality()
-        {
-            foreach (var item in Items)
-            {
-                var strategy = new ItemQualityUpdateStrategyFactory().Create(item);
-                strategy.UpdateQuality(item);
-                /*if (Items[i].Name != "Good Wine" 
-                    && Items[i].Name != "Backstage passes for Re:factor"
-                    && Items[i].Name != "Backstage passes for HAXX")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "B-DAWG Keychain")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes for Re:factor"
-                        || Items[i].Name == "Backstage passes for HAXX")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "B-DAWG Keychain")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Good Wine")
-                    {
-                        if (Items[i].Name != "Backstage passes for Re:factor"
-                            && Items[i].Name != "Backstage passes for HAXX")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "B-DAWG Keychain")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }*/
-            }
+            var strategy = new ItemQualityUpdateStrategyFactory().Create(item);
+            strategy.UpdateQuality(item);
         }
     }
 }
